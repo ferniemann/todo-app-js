023032fad964d11e6096f85ed1da57d6883b656a
Ferdinand Niemann <kontakt@ferdinand-niemann.eu>
2021-07-21 00:18:07 +0200

    Refactor: Changed the data structure
    Changed the data structure. Before I created the tasks list from the input value. Now instead I take the data from the array in which the input value is being pushed.

--- script.js
+++ script.js
@@ -8,6 +8,18 @@ let tasks = []
 
 btnAdd.disabled = true
 
+function isChecked() {
+    for (let i = 0; i< tasks.length; i++) {
+        const id = tasks[i].id
+        const checkbox = document.getElementById(id+"-item")
+        const done = tasks[i].done
+    
+        if (done === true) {
+            checkbox.setAttribute("checked", "checked")
+        } else checkbox.removeAttribute("checked", "checked")
+    }
+}
+
 text.addEventListener("input", function() {
     if (text.value.trim().replace(/ /g, "").length < 5) {
         btnAdd.disabled = true
@@ -21,7 +33,7 @@ console.log(tasks);
 btnAdd.addEventListener("click", function(e){
     e.preventDefault()
 
-    tasksList.classList.add("tasks")
+    tasksList.innerHTML = ""
 
     let textValue = text.value.trim().replace(/ +/g, " ")
     const id = textValue.trim().replace(/ /g, "-").toLowerCase()
@@ -33,70 +45,108 @@ btnAdd.addEventListener("click", function(e){
 
     tasks.push(taskItem)
 
-    const newLi = document.createElement("li")
-    newLi.taskObj = taskItem
-    console.log(newLi.taskObj);
-    newLi.setAttribute("id", id)
-    tasksList.appendChild(newLi)
+    // const newLi = document.createElement("li")
+    // newLi.taskObj = taskItem
+    // console.log(newLi.taskObj);
+    // newLi.setAttribute("id", id)
+    // tasksList.appendChild(newLi)
 
-    const checkbox = document.createElement("input")
-    checkbox.setAttribute("type", "checkbox")
-    checkbox.setAttribute("id", id+"-item")
-    newLi.appendChild(checkbox)
+    // const checkbox = document.createElement("input")
+    // checkbox.setAttribute("type", "checkbox")
+    // checkbox.setAttribute("id", id+"-item")
+    // newLi.appendChild(checkbox)
 
-    const label = document.createElement("label")
-    label.setAttribute("for", id+"-item")
-    const textNode = document.createTextNode(textValue)
-    label.append(textNode)
-    newLi.appendChild(label)
+    // const label = document.createElement("label")
+    // label.setAttribute("for", id+"-item")
+    // const textNode = document.createTextNode(textValue)
+    // label.append(textNode)
+    // newLi.appendChild(label)
 
-    newP = document.createElement("p")
-    newP.setAttribute("id", id+"-delete")
-    newP.setAttribute("class", "delete")
-    newP.innerHTML = "X"
-    btnDelete = newLi.appendChild(newP)
+    // newP = document.createElement("p")
+    // newP.setAttribute("id", id+"-delete")
+    // newP.setAttribute("class", "delete")
+    // newP.innerHTML = "X"
+    // btnDelete = newLi.appendChild(newP)
     
-    btnDelete.addEventListener("click", function(){
-        newLi.remove()
-    })
+    // btnDelete.addEventListener("click", function(){
+    //     newLi.remove()
+    // })
+
+    createTaskList()
+    isChecked()
 
     text.value = ""
     btnAdd.disabled = true
 })
 
+function createTaskList() {
+    for (let i = 0; i < tasks.length; i++) {
+        const title = tasks[i].title
+        const id = title.trim().replace(/ /g, "-").toLowerCase()
+        const newLi = document.createElement("li")
+        newLi.setAttribute("id", id)
+        tasksList.appendChild(newLi)
+
+        const checkbox = document.createElement("input")
+        checkbox.setAttribute("type", "checkbox")
+        checkbox.setAttribute("id", id+"-item")
+        newLi.appendChild(checkbox)
+
+        const label = document.createElement("label")
+        label.setAttribute("for", id+"-item")
+        const textContent = document.createTextNode(title)
+        label.append(textContent)
+        newLi.appendChild(label)
+    }
+}
+
 tasksList.addEventListener("change", function(e) {
     const checked = e.target.checked
     const id = e.target.parentNode.id
+    const checkbox = document.getElementById(id+"-item")
     tasks.find(x => x.id === id).done = checked
+
+    if (tasks.find(x => x.id === id).done === true) {
+        checkbox.setAttribute("checked", "checked")
+    } else {
+        checkbox.removeAttribute("checked", "checked")
+    }
 })
 
 function filterDone() {
-    for (let i = 0; i < tasksList.children.length; i++) {
-        const taskObj = tasksList.children[i].taskObj
-        if (taskObj.done === false) {
-            tasksList.children[i].hidden = true
-        } else {
-            tasksList.children[i].hidden = false
+    for (let i = 0; i < tasks.length; i++) {
+        const task = tasks[i]
+        title = task.title
+        id = title.replace(/ /g, "-").toLowerCase()
+        listElement = document.getElementById(id)
+        listElement.hidden = false
+        
+        if (task.done === false) {
+        listElement.hidden = true
         }
     }
 }
 
 function filterAll() {
-    for (let i = 0; i < tasksList.children.length; i++) {
-        const taskObj = tasksList.children[i].taskObj
-        if (tasksList.children[i].hidden === true) {
-            tasksList.children[i].hidden = false
-        }
+    for (let i = 0; i < tasks.length; i++) {
+        const task = tasks[i]
+        title = task.title
+        id = title.replace(/ /g, "-").toLowerCase()
+        listElement = document.getElementById(id)
+        listElement.hidden = false
     }
 }
 
 function filterOpen() {
-    for (let i = 0; i < tasksList.children.length; i++) {
-        const taskObj = tasksList.children[i].taskObj
-        if (taskObj.done === false) {
-            tasksList.children[i].hidden = false
-        } else {
-            tasksList.children[i].hidden = true
+    for (let i = 0; i < tasks.length; i++) {
+        const task = tasks[i]
+        title = task.title
+        id = title.replace(/ /g, "-").toLowerCase()
+        listElement = document.getElementById(id)
+        listElement.hidden = false
+        
+        if (task.done === true) {
+        listElement.hidden = true
         }
     }
 }
